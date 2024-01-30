791fbd2bd30850db356f7f38752bd97bfced093f
Nick Neely <neelynickolas@gmail.com>
2024-01-29 23:58:23 -0600

    Personalized site, added contact page and form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://nickneely.dev",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    edgeMiddleware: true,
+    webAnalytics: {
+      enabled: true
+    },
+    maxDuration: 8
+  })
 });
